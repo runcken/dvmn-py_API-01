51088457dd31de97232fac59e72ce56c57ca5d5e
Alexey Nikiforov <runcken@gmail.com>
2025-07-01 18:56:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+
+
+cities = ('svo', 'london', 'череповец')
+
+
+def get_weather():
+    for city in cities:
+        url = (
+            'http://wttr.in/{city}?lang=ru&3mnqMT&%20HTTP/1.1'
+            .format(city=city)
+            )
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.text)
+
+
+def main():
+    get_weather()
+
+
+if __name__ == '__main__':
+    main()
